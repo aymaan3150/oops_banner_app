e75fca3bdb46c9acf7960456a4e1fe5b0809d600
aymaan3150 <am3150@srmist.edu.in>
2026-02-16 14:17:55 +0530

    Update oopsbanner.java

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,3 +1,10 @@
 public class oopsbanner {
-    
+    public static void main(String[] args) {
+        // Use Case 2: Render OOPS as Banner using Print Statements
+        System.out.println("  ***    ***   ****    *****  ");
+        System.out.println(" * * *  *   *  *   *  *     ");
+        System.out.println(" * * *  *   *  ****    ****    ");
+        System.out.println(" * * *  *   *  *            *   ");
+        System.out.println("  ***    ***   *       *****       ");
+    }
 }
